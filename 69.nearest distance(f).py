2dc3aeda3ce3c486fc58782f3e6ae9ad2f45c03d
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-05-31 05:44:30 -0300

    Add files via upload

---
+++ 69.nearest distance(f).py
@@ -0,0 +1,8 @@
+import math
+def distance (p):
+    return math.sqrt(  p[0]**2 +  p[1]**2 )
+a = [ (0,1) , (1,2) , (2,4) ,(3,7) ]
+b = [distance(i) for i in a]
+m = d.index(main(d) )
+print ("point {} has the min distance {}."\
+        .format(a[m],main(d)) )
