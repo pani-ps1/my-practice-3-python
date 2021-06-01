015bad251f51fb57c6785be207b038a96b2937d0
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-06-01 11:36:25 -0300

    Add files via upload

---
+++ 71.f string.py
@@ -0,0 +1,31 @@
+a = 3;b = 2;c = 3
+print(a)
+print("="*25)
+print(b)
+print("="*25)
+print(c)
+
+
+print("sajjad",end="\n",end=" ")
+print("rahmani",end="#")
+
+a=2
+b=3
+print( "{}*{}= {}".format(a,b,a*b) )
+
+print( f"{a}*{b}= {a*b}" )
+
+
+------------------
+
+name = input("enter your name: ")
+print(f"your name is {name}.")
+
+
+--------------------
+
+#input
+
+a= input("enter your name= ")
+print(a)
+print (type(a))
