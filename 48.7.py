e487399083fe4e792c2ce851627a9f08fc9ba834
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-07-13 03:37:46 -0400

    Add files via upload

---
+++ 48.7.py
@@ -0,0 +1,12 @@
+payam_be_sheema = "salam bia inja darim python yad migirim"
+
+payam_eslah_shode = ""
+
+for letter in payam_be_sheema:
+    if letter == "i":
+        payam_eslah_shode += "ee"
+    else:
+        payam_eslah_shode += letter
+
+
+print(payam_eslah_shode)
